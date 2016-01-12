29cf5c045c2eadc8f30f88d2cc5a7b04eb765b1c
Ali <alexandrafitzgerald@Alexandras-MacBook-Pro.local>
2016-01-12 10:43:36 -0500

    carousel works

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,73 @@
-$('.listing-modal').on('show.bs.modal', function (e) {
-  console.log('fetching photos');
-})
+
+$(document).ready(function() {
+  // var listingId;
+  $('#listing').on('click', function(e) {
+    console.log($(e.currentTarget));
+    console.log('go');
+    var listingId = $(e.currentTarget).parent()[0].id;
+    var url = console.log(listingId);
+    $.ajax({
+      type: 'POST',
+      data: {
+        listingId: listingId
+      },
+      url: 'http://localhost:8080/sales',
+      success: function(data) {
+
+        if (data.length === 0) {
+          $('#myModal-' + listingId + ' .carousel').hide();
+          return;
+        }
+
+        for (var i = 0; i < data.length; i++) {
+          console.log('called');
+          var url = data[i].Url[0];
+          var id = data[i].ID[0]
+          var html1 = '',
+            html2 = '';
+          if (i === 0) {
+            html1 = "<li data-target='#carousel-example-generic' data-slide-to='" + i + "' class='active'></li>";
+            console.log('this should only happen once');
+            html2 = "<div class='item active'><img src='" + url + "' alt='photo-" + id + "'></div>"
+          } else {
+            html1 = "<li data-target='#carousel-example-generic' data-slide-to='" + i + "'></li>";
+            html2 = "<div class='item'><img src='" + url + "' alt='photo-" + id + "'></div>"
+          }
+
+          $("#myModal-" + listingId + " #indicators").append(html1);
+          $("#myModal-" + listingId + " #carousel-body").append(html2);
+        }
+      }
+    });
+  });
+
+
+
+
+  // $(".next-btn").click(function(event) {
+  //   event.preventDefault();
+  // });
+
+  // $('.next-btn').on('click', function(e) {
+  //   console.log('clicked');
+  //   $(".target").hide();
+
+  //   $.ajax({
+  //     type: 'GET',
+  //     dataType: "json",
+  //     url: 'http://localhost:8080/sales',
+  //     success: function(data) {
+  //       console.log(data);
+  //       console.log('MOREsuccess');
+  //     }
+  //   })
+
+// });
+});
+
+
+
+function goBack() {
+  window.history.back();
+  console.log('went back');
+}
--- server.js
+++ server.js
@@ -35,6 +35,7 @@ mailer.extend(app, {
 });
 
 
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({
@@ -66,29 +67,20 @@ var request = require('request');
 
 
 
-
 app.get('/', function(req, res) {
   var url = 'https://www.yougotlistings.com/api/rentals/search.php?key=Z6x3y2AYQIVNjFkJ1C8alfcMGEtzuKpgLHn5vRrT&street_name=stephen&include_mls=1';
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
-
-
-
       parseString(body, function(err, result) {
-
-        console.log(result.YGLResponse.Listings[0].Listing[0]);
-
 
         res.render('listings', {
           listings: result.YGLResponse.Listings[0].Listing[0]
         });
-
       });
     }
   });
-
 });
+
 
 
 
@@ -97,26 +89,16 @@ app.get('/hemenway', function(req, response) {
   var url = 'https://www.yougotlistings.com/api/rentals/search.php?key=Z6x3y2AYQIVNjFkJ1C8alfcMGEtzuKpgLHn5vRrT&street_name=hemenway&include_mls=1';
   var res = response;
   console.log(request.body);
-
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
-
-
-
       parseString(body, function(err, result) {
-
         console.log(result.YGLResponse.Listings[0].Listing[6]);
-
-
         res.render('hemenway', {
           listings: result.YGLResponse.Listings[0].Listing
         });
-
       });
     }
   });
-
 });
 
 app.get('/gainsborough', function(req, response) {
@@ -126,13 +108,8 @@ app.get('/gainsborough', function(req, response) {
 
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
-
-
-
       parseString(body, function(err, result) {
-
-
+        //console.log(result.YGLResponse.Listings[0].Listing);
         res.render('gainsborough', {
           listings: result.YGLResponse.Listings[0].Listing
         });
@@ -143,32 +120,36 @@ app.get('/gainsborough', function(req, response) {
 
 });
 
+app.get('/sales', function(req, res) {
+  res.render('sales');
+});
 
-app.get('/gainsborough/sales', function(req, response) {
-  var url = 'https://www.yougotlistings.com/api/rentals/search.php?key=Z6x3y2AYQIVNjFkJ1C8alfcMGEtzuKpgLHn5vRrT&street_name=gainsborough&include_mls=1';
+app.post('/sales', function(req, response) {
+  console.log('pressed');
+  var lid = req.body.listingId;
+  console.log('Listing id is ' + lid);
+  var url = 'https://www.yougotlistings.com/api/photos/search.php?&key=Z6x3y2AYQIVNjFkJ1C8alfcMGEtzuKpgLHn5vRrT&listing_id=' + lid;
   var res = response;
-  console.log(request.body);
-
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
-
-
-
       parseString(body, function(err, result) {
+        var photos = null;
+        if (result.YGLResponse.Photos) {
+          photos = result.YGLResponse.Photos[0].Photo;
+        } else {
+          console.log(result);
+          console.log('No photos')
+          photos = null;
+        }
 
-        console.log(result.YGLResponse.Listings[0].Listing[6]);
-
-
-        res.render('sales', {
-          listings: result.YGLResponse.Listings[0].Listing
-        });
-
+        res.send(photos);
+        res.end();
       });
     }
   });
-
 });
+
+
 
 app.get('/symphony', function(req, response) {
   var url = 'https://www.yougotlistings.com/api/rentals/search.php?key=Z6x3y2AYQIVNjFkJ1C8alfcMGEtzuKpgLHn5vRrT&street_name=symphony&include_mls=1';
@@ -177,23 +158,14 @@ app.get('/symphony', function(req, response) {
 
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
-
-
-
       parseString(body, function(err, result) {
-
         console.log(result.YGLResponse.Listings[0].Listing[6]);
-
-
         res.render('symphony', {
           listings: result.YGLResponse.Listings[0].Listing
         });
-
       });
     }
   });
-
 });
 
 app.get('/stephen', function(req, response) {
@@ -203,10 +175,6 @@ app.get('/stephen', function(req, response) {
 
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
-
-
-
       parseString(body, function(err, result) {
 
         console.log(result.YGLResponse.Listings[0].Listing);
