46a6cf7d12ca45260ed9c3a6925ddb95e9b167a2
Ali <alexandrafitzgerald@Alexandras-MacBook-Pro.local>
2016-01-23 18:32:17 -0500

    fixed carousel error

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,15 @@
 
 $(document).ready(function() {
 
-  $('.carousel').carousel({
-    interval: false
-  });
 
-  $('.carousel-control.left').click(function() {
-    $('#carousel-example-generic').carousel('prev');
-    console.log('left');
-  });
 
-  $('.carousel-control.right').click(function() {
-    $('#carousel-example-generic').carousel('next');
-    console.log('right');
-  });
+
+
+
 
 
   // var listingId;
   $('.listing').on('click', function(e) {
-    console.log($(e.currentTarget));
-    console.log('go');
     var listingId = $(e.currentTarget).parent()[0].id;
     var url = console.log(listingId);
     $.ajax({
@@ -27,73 +17,55 @@ $(document).ready(function() {
       data: {
         listingId: listingId
       },
-      url: 'http://localhost:8080/sales',
-      //url: 'https://mprealestate.herokuapp.com/sales',
+      //url: 'http://localhost:8080/sales',
+      url: 'https://mprealestate.herokuapp.com/sales',
       success: function(data) {
 
-        $("#myModal-" + listingId).on('shown.bs.modal', function() {
-
-          if (data.length === 0) {
-            $('#myModal-' + listingId + ' .carousel').hide();
-            return;
-          }
 
 
+        if (data.length === 0) {
+          $('#myModal-' + listingId + ' .carousel').hide();
+          return;
+        }
 
+        $('.carousel-control.right').click(function() {
+          $('#myModal-' + listingId).carousel('next');
+          console.log('right');
 
-          for (var i = 0; i < data.length; i++) {
-
-            var url = data[i].Url[0];
-            var id = data[i].ID[0]
-            var html1 = '',
-              html2 = '';
-            if (i === 0) {
-              html1 = "<li data-target='#carousel-example-generic' data-slide-to='" + i + "' class='active'></li>";
-              console.log('this should only happen once');
-              html2 = "<div class='item active'><img src='" + url + "' alt='photo-" + id + "'></div>";
-
-            } else {
-              html1 = "<li data-target='#carousel-example-generic' data-slide-to='" + i + "'></li>";
-              html2 = "<div class='item'><img src='" + url + "' alt='photo-" + id + "'></div>";
-
-            }
-
-
-            $("#myModal-" + listingId + " #indicators").append(html1);
-            $("#myModal-" + listingId + " #carousel-body").append(html2);
-
-
-
-
-          }
         });
-      }
+        $('.carousel-control.left').click(function() {
+          $('#myModal-' + listingId).carousel('prev');
+          console.log('right');
+
+        });
+
+        for (var i = 0; i < data.length; i++) {
+
+          var url = data[i].Url[0];
+          var id = data[i].ID[0]
+          var html1 = '',
+            html2 = '';
+          if (i === 0) {
+            html1 = "<li data-target='#myModal-{{ID}}' data-slide-to='" + i + "' class='active'></li>";
+            console.log('this should only happen once');
+            html2 = "<div class='item active'><img src='" + url + "' alt='photo-" + id + "'></div>";
+            console.log("this one is active " + i);
+
+          } else {
+            html1 = "<li data-target='#myModal-{{ID}}' data-slide-to='" + i + "'></li>";
+            html2 = "<div class='item'><img src='" + url + "' alt='photo-" + id + "'></div>";
+
+          }
+
+
+          $("#myModal-" + listingId + " #indicators").append(html1);
+          $("#myModal-" + listingId + " #carousel-body").append(html2);
+
+        }
+
+      },
     });
   });
-
-
-
-
-
-  // $(".next-btn").click(function(event) {
-  //   event.preventDefault();
-  // });
-
-  // $('.next-btn').on('click', function(e) {
-  //   console.log('clicked');
-  //   $(".target").hide();
-
-  //   $.ajax({
-  //     type: 'GET',
-  //     dataType: "json",
-  //     url: 'http://localhost:8080/sales',
-  //     success: function(data) {
-  //       console.log(data);
-  //       console.log('MOREsuccess');
-  //     }
-  //   })
-
-// });
 });
 
 
