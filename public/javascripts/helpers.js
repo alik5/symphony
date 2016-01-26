95e6705f1a6740182f8d7f421895c3c3a4f3049d
Ali <alexandrafitzgerald@Alexandras-MacBook-Pro-2.local>
2016-01-25 21:59:19 -0500

    styling

---
+++ public/javascripts/helpers.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+  $('#preloader_4')
+    .hide()
+    .ajaxStart(function() {
+      $(this).show();
+    })
+    .ajaxStop(function() {
+      $(this).hide();
+    });
+  // PAGE IS FULLY LOADED  
+  // FADE OUT YOUR OVERLAYING DIV
+  $('#preloader_4').fadeOut();
+
+  $('div.hidden').fadeIn(1000).removeClass('hidden');
+});
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,15 @@
 
 $(document).ready(function() {
+  var visible = true;
+
+  $("#preloader_4").hide();
 
 
 
+  $('.trans').click(function() {
+    $('#body').addClass('magictime vanishOut');
+    $("#preloader_4").show();
+  });
 
 
 
@@ -10,6 +17,7 @@ $(document).ready(function() {
 
   // var listingId;
   $('.listing').on('click', function(e) {
+
     var listingId = $(e.currentTarget).parent()[0].id;
     var url = console.log(listingId);
     $.ajax({
@@ -17,11 +25,9 @@ $(document).ready(function() {
       data: {
         listingId: listingId
       },
-      //url: 'http://localhost:8080/sales',
-      url: 'https://mprealestate.herokuapp.com/sales',
+      url: 'http://localhost:8080/sales',
+      //url: 'https://mprealestate.herokuapp.com/sales',
       success: function(data) {
-
-
 
         if (data.length === 0) {
           $('#myModal-' + listingId + ' .carousel').hide();
@@ -39,30 +45,31 @@ $(document).ready(function() {
 
         });
 
-        for (var i = 0; i < data.length; i++) {
+        if (visible) {
 
-          var url = data[i].Url[0];
-          var id = data[i].ID[0]
-          var html1 = '',
-            html2 = '';
-          if (i === 0) {
-            html1 = "<li data-target='#myModal-{{ID}}' data-slide-to='" + i + "' class='active'></li>";
-            console.log('this should only happen once');
-            html2 = "<div class='item active'><img src='" + url + "' alt='photo-" + id + "'></div>";
-            console.log("this one is active " + i);
+          for (var i = 0; i < data.length; i++) {
 
-          } else {
-            html1 = "<li data-target='#myModal-{{ID}}' data-slide-to='" + i + "'></li>";
-            html2 = "<div class='item'><img src='" + url + "' alt='photo-" + id + "'></div>";
+            var url = data[i].Url[0];
+            var id = data[i].ID[0]
+            var html1 = '',
+              html2 = '';
+            if (i === 0) {
+              html1 = "<li data-target='#myModal-{{ID}}' data-slide-to='" + i + "' class='active'></li>";
+              console.log('this should only happen once');
+              html2 = "<div class='item active'><img src='" + url + "' alt='photo-" + id + "'></div>";
+              console.log("this one is active " + i);
 
+            } else {
+              html1 = "<li data-target='#myModal-{{ID}}' data-slide-to='" + i + "'></li>";
+              html2 = "<div class='item'><img src='" + url + "' alt='photo-" + id + "'></div>";
+
+            }
+
+            $("#myModal-" + listingId + " #indicators").append(html1);
+            $("#myModal-" + listingId + " #carousel-body").append(html2);
+            visible = false;
           }
-
-
-          $("#myModal-" + listingId + " #indicators").append(html1);
-          $("#myModal-" + listingId + " #carousel-body").append(html2);
-
         }
-
       },
     });
   });
