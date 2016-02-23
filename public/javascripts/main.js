061f58d684eca58bb12089368a8529508f23f02a
Ali <alexandrafitzgerald@Alexandras-MacBook-Pro-2.local>
2016-02-23 18:10:29 -0500

    moved typeform function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,21 +14,6 @@ $(document).ready(function() {
 
 
 
-  (function() {
-    var qs, js, q, s,
-      d = document,
-      gi = d.getElementById,
-      ce = d.createElement,
-      gt = d.getElementsByTagName,
-      id = 'typef_orm',
-      b = 'https://s3-eu-west-1.amazonaws.com/share.typeform.com/';
-    if (!gi.call(d, id)) {
-      js = ce.call(d, 'script');
-      js.id = id;
-      js.src = b + 'widget.js';
-      q = gt.call(d, 'script')[0];q.parentNode.insertBefore(js, q)
-    }
-  })()
 
 
 
@@ -130,6 +115,21 @@ $(document).ready(function() {
       url: urlAddress,
       //url: 'http://www.saintstephenstreet.com/',
       success: function(data) {
+          (function() {
+    var qs, js, q, s,
+      d = document,
+      gi = d.getElementById,
+      ce = d.createElement,
+      gt = d.getElementsByTagName,
+      id = 'typef_orm',
+      b = 'https://s3-eu-west-1.amazonaws.com/share.typeform.com/';
+    if (!gi.call(d, id)) {
+      js = ce.call(d, 'script');
+      js.id = id;
+      js.src = b + 'widget.js';
+      q = gt.call(d, 'script')[0];q.parentNode.insertBefore(js, q)
+    }
+  })()
 
 
 
